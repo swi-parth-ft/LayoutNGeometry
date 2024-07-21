547dfcc1ecb058142f25662ee442f76088506c8b
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-21 11:07:09 -0400

    Understanding frames and coordinates inside GeometryReader

--- LayoutNGeometry/ContentView.swift
+++ LayoutNGeometry/ContentView.swift
@@ -6,33 +6,41 @@
 //
 
 import SwiftUI
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            d[.top]
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
         }
     }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
 }
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Text("Hey, SwiftUI")
-            .font(.largeTitle)
-                
-            GeometryReader { proxy in
-                Circle()
-                    .fill(.orange)
-                    .frame(width: proxy.size.width * 0.8)
-                    .frame(width: proxy.size.width, height: proxy.size.height)
-            }
-        }
-            
-            
-        
-        
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
     }
 }
 
