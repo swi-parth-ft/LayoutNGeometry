9e5ccc9f8a9b5276f67f39e3c4c344e35b629e47
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-21 10:03:11 -0400

    Initial Commit

---
+++ LayoutNGeometry/LayoutNGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutNGeometryApp.swift
+//  LayoutNGeometry
+//
+//  Created by Parth Antala on 2024-07-21.
+//
+
+import SwiftUI
+
+@main
+struct LayoutNGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
