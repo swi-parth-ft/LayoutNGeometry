e6d586bdfb2a7ccf11b8928b4550a9315a365425
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-21 10:55:18 -0400

    Use GeometryReader to resizing views and alignment

--- LayoutNGeometry/ContentView.swift
+++ LayoutNGeometry/ContentView.swift
@@ -18,70 +18,18 @@ extension VerticalAlignment {
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .lastTextBaseline) {
-            Text("Live")
-                .font(.caption)
-            
-            Text("long")
-            
-            Text("and")
-                .font(.title)
-            
-            Text("prosper")
-                .font(.largeTitle)
-        }
-        
-        VStack(alignment: .leading) {
-            Text("Hello, Swift!")
-                .alignmentGuide(.leading) { d in
-                    d[.trailing]
-                }
-            Text("This is amazing language to learn")
-        }
-        .background(.red)
-        .frame(width: 400, height: 100)
-        .background(.orange)
-        
-        VStack(alignment: .leading) {
-            ForEach(0..<10) { number in
-                Text("Number: \(number)")
-                    .alignmentGuide(.leading) { _ in
-                        Double(number) * -10
-                    }
-            }
-        }
-        
-        Spacer()
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@parpods")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[.top]
-                    }
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("Name:")
+        HStack {
+            Text("Hey, SwiftUI")
+            .font(.largeTitle)
                 
-                Text("PARTH ANTALA")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[.top]
-                    }
-                    .font(.largeTitle)
-                
+            GeometryReader { proxy in
+                Circle()
+                    .fill(.orange)
+                    .frame(width: proxy.size.width * 0.8)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
             }
         }
-        
-        Text("Hey, SwiftUI")
-            .offset(x: 100, y: 50)
-            .background(.green)
-        
-        Text("Hello, Swift!")
-            .background(.red)
-            .position(x: 100, y: 100)
+            
             
         
         
