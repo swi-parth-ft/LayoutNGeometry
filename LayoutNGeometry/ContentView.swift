c1553deed9484be569ec421b5de502c577ea50e9
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-21 10:38:10 -0400

    Custom alignment guide

--- LayoutNGeometry/ContentView.swift
+++ LayoutNGeometry/ContentView.swift
@@ -6,6 +6,15 @@
 //
 
 import SwiftUI
+extension VerticalAlignment {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
 
 struct ContentView: View {
     var body: some View {
@@ -30,7 +39,7 @@ struct ContentView: View {
             Text("This is amazing language to learn")
         }
         .background(.red)
-        .frame(width: 400, height: 400)
+        .frame(width: 400, height: 100)
         .background(.orange)
         
         VStack(alignment: .leading) {
@@ -39,6 +48,30 @@ struct ContentView: View {
                     .alignmentGuide(.leading) { _ in
                         Double(number) * -10
                     }
+            }
+        }
+        
+        Spacer()
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@parpods")
+                    .alignmentGuide(.midAccountAndName) { d in
+                        d[.top]
+                    }
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("Name:")
+                
+                Text("PARTH ANTALA")
+                    .alignmentGuide(.midAccountAndName) { d in
+                        d[.top]
+                    }
+                    .font(.largeTitle)
+                
             }
         }
     }
